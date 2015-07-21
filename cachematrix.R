82f8138551c3777eea67b47833e18769ac31f449
Bernard Renger <renger@research.att.com>
2015-07-21 16:45:34 -0400

    Got cachematrix working

--- cachematrix.R
+++ cachematrix.R
@@ -1,41 +1,29 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-makeVector <- function(x = numeric()) {
-        m <- NULL
-        set <- function(y) {
-                x <<- y
-                m <<- NULL
-        }
-        get <- function() x
-        setmean <- function(mean) m <<- mean
-        getmean <- function() m
-        list(set = set, get = get,
-             setmean = setmean,
-             getmean = getmean)
-}
-
-cachemean <- function(x, ...) {
-        m <- x$getmean()
-        if(!is.null(m)) {
-                message("getting cached data")
-                return(m)
-        }
-        data <- x$get()
-        m <- mean(data, ...)
-        x$setmean(m)
-        m
-}
-
-## Write a short comment describing this function
+## Cache inverted matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+		m<-NULL
+		set<-function(y){
+				x<<-y
+				m<<-NULL
+		}
+		get<-function() x
+		setinv<-function(solve) m<<- solve
+		getinv<-function() m
+		list(set=set, get=get,
+				setinv=setinv,
+				getinv=getinv)
 }
 
+## Retrieve inverted matrix cache if available, otherwise calculate it
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x=matrix(), ...) {
+		m<-x$getinv()
+		if(!is.null(m)){
+				message("getting cached data")
+				return(m)
+		}
+		matrix<-x$get()
+		m<-solve(matrix, ...)
+		x$setinv(m)
+		m
 }
